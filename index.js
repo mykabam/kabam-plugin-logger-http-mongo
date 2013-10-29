e65a7e797ab4c49fb8f0d6a64be70698f1941849
Alexander Petrovich <chopachom@gmail.com>
2013-10-29 20:55:22 +0400

    chore: use new model definition interface

--- index.js
+++ index.js
@@ -1,33 +1,33 @@
 exports.name = 'kabamPluginLoggerHttpMongo';
 
 exports.model = {
-  'httpLog':function(kabam){
+  'httpLog': function (kabam) {
 
-  var httpLogSchema = new kabam.mongoose.Schema({
-    "timestamp": {type: Date, default: new Date()},
-    "duration": {type: Number, min: 1},
-    "statusCode":{type: Number, min: 1, max: 1000},
-    "method": {type: String, match: /GET|POST|PUT|DELETE|OPTIONS/i},
-    "ip": String,
-    "uri": {type: String, match: /\/.*/},
-    "username": {type: String, default: null},
-    "email":  {type: String, default: null}
-  });
+    var httpLogSchema = new kabam.mongoose.Schema({
+      "timestamp": {type: Date, default: new Date()},
+      "duration": {type: Number, min: 1},
+      "statusCode": {type: Number, min: 1, max: 1000},
+      "method": {type: String, match: /GET|POST|PUT|DELETE|OPTIONS/i},
+      "ip": String,
+      "uri": {type: String, match: /\/.*/},
+      "username": {type: String, default: null},
+      "email": {type: String, default: null}
+    });
 
-  httpLogSchema.index({
-    timestamp: 1,
-    ip: 1,
-    uri: 1,
-    username: 1
-  });
+    httpLogSchema.index({
+      timestamp: 1,
+      ip: 1,
+      uri: 1,
+      username: 1
+    });
 
-  return kabam.mongoConnection.model('httpLog', httpLogSchema);
+    return httpLogSchema;
   }
 };
 
 //the middleware for logging
-exports.middleware = function(kabam){
-  return function(request,response,next){
+exports.middleware = function (kabam) {
+  return function (request, response, next) {
     response.on('finish', function () {
       request.model.httpLog.create({
         'startTime': request._startTime,
@@ -38,8 +38,8 @@ exports.middleware = function(kabam){
         'uri': request.originalUrl,
         'username': (request.user ? request.user.username : null),
         'email': (request.user ? request.user.email : null)
-      },function(err, dataSaved){
-        if(err) throw err;
+      }, function (err, dataSaved) {
+        if (err) throw err;
         //console.log(dataSaved);
       });
     });
